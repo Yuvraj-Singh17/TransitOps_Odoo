673fa90dd76c2ec960fbf7df019c2b5e4a5489ef
Yuvraj <yuvivishu17@gmail.com>
2026-07-12 17:49:06 +0530

    final

--- Backend/server.js
+++ Backend/server.js
@@ -100,4 +100,3 @@ app.listen(PORT, () => {
   console.log(`📡 Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
 });
 
-module.exports = app; // Or "export default app;" if you are using ES modules
